a022961503b2bc86f259ece29ab80f6efc89131b
Jonatas Alvarenga Maximiano <maximianojonatas@gmail.com>
2018-05-24 15:31:47 -0300

    Update CalculateInstallmentsTest.java

--- src/test/java/me/pagarme/CalculateInstallmentsTest.java
+++ src/test/java/me/pagarme/CalculateInstallmentsTest.java
@@ -36,7 +36,7 @@ public class CalculateInstallmentsTest extends BaseTest {
         List<Installment> installments = calculation.calculateInstallments(10000, 3, 1.5, 12);
         Assert.assertEquals(10750, installments.get(4).getAmount());
         Assert.assertEquals(5, installments.get(4).getInstallment());
-        Assert.assertEquals(2150, installments.get(4).getInstallment_amount());
+        Assert.assertEquals(2150, installments.get(4).getInstallmentAmount());
     }
 
     @Test
@@ -44,7 +44,7 @@ public class CalculateInstallmentsTest extends BaseTest {
         List<Installment> installments = calculation.calculateInstallments(90000, 3, 0, 3);
         Assert.assertEquals(90000, installments.get(2).getAmount());
         Assert.assertEquals(3, installments.get(2).getInstallment());
-        Assert.assertEquals(30000, installments.get(2).getInstallment_amount());
+        Assert.assertEquals(30000, installments.get(2).getInstallmentAmount());
 
     }
 

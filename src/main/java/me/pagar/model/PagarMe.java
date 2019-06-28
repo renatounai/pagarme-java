5ae0ec578e28c9b0a4f7d1394f9a734935154ab2
Leonam Pereira Dias <leonam.pd@gmail.com>
2019-06-28 15:56:11 -0300

    restclient: inform user-agent during requests

--- src/main/java/me/pagar/model/PagarMe.java
+++ src/main/java/me/pagar/model/PagarMe.java
@@ -13,6 +13,8 @@ public abstract class PagarMe {
 
     public static final String API_VERSION = "1";
 
+    public static final String VERSION = "1.5.8";
+
     public static final String HMAC_MD5_ALGORITHM = "HmacMD5";
 
     public static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";

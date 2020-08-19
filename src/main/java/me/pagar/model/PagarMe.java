b0887205d70af77fba20fbefe3f116c489a8ee2b
Lucas Lyoji <lucas.omokawa@gmail.com>
2020-08-19 15:14:24 -0300

    update SDK version

--- src/main/java/me/pagar/model/PagarMe.java
+++ src/main/java/me/pagar/model/PagarMe.java
@@ -13,7 +13,7 @@ public abstract class PagarMe {
 
     public static final String API_VERSION = "1";
 
-    public static final String VERSION = "1.5.8";
+    public static final String VERSION = "2.0.0";
 
     public static final String HMAC_MD5_ALGORITHM = "HmacMD5";
 
